1769c6cf93fff898d0248fc83763f7dc92c9e1d6
Interchained <president@worldvaporexpo.com>
2022-06-16 04:56:14 -0400

    add web3 eth call to eth_getBlockByNumber, asynchronously get block by top block number

--- signors.js
+++ signors.js
@@ -1,4 +1,7 @@
 const { ethers } = require("ethers");
+const Web3 = require('web3');
+// change the RPC if you want to switch networks / providers
+const web3 = new Web3('https://evm.cryptocurrencydevs.org');
 const cliqueMiner = (block) => {
   try {
     const keys = ["parentHash", "sha3Uncles", "miner", "stateRoot", "transactionsRoot", "receiptsRoot", "logsBloom", "difficulty", "number", "gasLimit", "gasUsed", "timestamp", "extraData", "mixHash", "nonce", "baseFeePerGas"];
@@ -33,30 +36,13 @@ const cliqueMiner = (block) => {
     return block.miner;
   }
 };
-
-// web3 output from the blocks eth_getBlockByNumber
-const block = {
-    baseFeePerGas: 7,
-    difficulty: 2,
-    extraData: "0xd883010a11846765746888676f312e31372e36856c696e75780000000000000042bc5273624489d846ba7bb9723ec9ea9af0c306b7993912a1e3b4d5aa1f73510c5c62e92542b8c28fedb34ca5263919edaf06a603f92a9883612c144e01c67f01",
-    gasLimit: 8000000,
-    gasUsed: 21000,
-    hash: "0x9523ded8fc6ae6fcf9e84940e3eec7e15a796f44674720379dfb4d9a6b506c53",
-    logsBloom: "0x00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
-    miner: "0x0000000000000000000000000000000000000000",
-    mixHash: "0x0000000000000000000000000000000000000000000000000000000000000000",
-    nonce: "0x0000000000000000",
-    number: 2196789,
-    parentHash: "0x10a4bc4b1643bd39ec859126d1197281da9f65eca6cb189a68877edeea563afc",
-    receiptsRoot: "0xf78dfb743fbd92ade140711c8bbc542b5e307f0ab7984eff35d751969fe57efa",
-    sha3Uncles: "0x1dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d49347",
-    size: 734,
-    stateRoot: "0xcdad7c1c03a62aa22c19c281a39cb9541de2f1140f21c45d170936b5f2a1153d",
-    timestamp: 1654685641,
-    totalDifficulty: 3835521,
-    transactions: ["0x05f105de8024f605e50df7010c033b8d0bd54d241a622ded96f7b69e7ad5d6d3"],
-    transactionsRoot: "0xbad9c8c528ee6ae544cb85a7b9a4ba818e0f576f445a85c741df05b76af7c5ee",
-    uncles: []
-};
-
-console.log("Signor: "+cliqueMiner(block)); // for this example 0x1B355B8e90159a23e1685Ae582C81bb3bF80E795
+// we establish there would soon be a block...
+let block;
+// web3 outputs top blocks eth call to eth_getBlockByNumber
+async function getBlock(block) {
+    let getBlockNumber = await web3.eth.getBlockNumber();
+    console.log("Block Number: "+getBlockNumber);
+    block = await web3.eth.getBlock(getBlockNumber);
+    console.log("Signor: "+cliqueMiner(block));
+}
+getBlock(block);
